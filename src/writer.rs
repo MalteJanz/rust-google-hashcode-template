9120b9dd710b6094e2dc343b9677481af4183eec
Malte Janz <malte.janz@yahoo.de>
2021-02-01 16:58:06 +0100

    Remove example implementation for a clean start with a new problem

--- src/writer.rs
+++ src/writer.rs
@@ -1,26 +1,16 @@
-use crate::{DataContext, PizzaDelivery};
+use crate::{DataContext};
 use std::io::Write;
 
-pub fn write_data(data_context: DataContext, pizza_deliveries: Vec<PizzaDelivery>) {
+pub fn write_data(data_context: DataContext) {
     let mut writer = data_context.file_context.create_writer();
 
     // write first line
+    /*
     writer
         .write_all(format!("{}\n", pizza_deliveries.len()).as_bytes())
         .unwrap();
 
-    // write other lines
-    for pizza_delivery in pizza_deliveries {
-        writer
-            .write_all(pizza_delivery.team_size.to_string().as_bytes())
-            .unwrap();
+     */
 
-        let ids_concatenated = pizza_delivery
-            .pizza_ids
-            .iter()
-            .fold(String::new(), |a, b| format!("{} {}", a, b));
-
-        writer.write_all(ids_concatenated.as_bytes()).unwrap();
-        writer.write_all(b"\n").unwrap();
-    }
+    // ...
 }
